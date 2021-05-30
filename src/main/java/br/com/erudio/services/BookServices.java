8319c93ca721c2c2158d431f1d3923680167f7c0
OakAnderson <andersonfelipe01@live.com>
2021-05-30 18:48:46 -0300

    Created book service

---
+++ src/main/java/br/com/erudio/services/BookServices.java
@@ -0,0 +1,53 @@
+package br.com.erudio.services;
+
+import br.com.erudio.converter.DozerConverter;
+import br.com.erudio.data.model.Book;
+import br.com.erudio.data.vo.v1.BookVO;
+import br.com.erudio.exception.ResourceNotFoundException;
+import br.com.erudio.repository.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServices {
+
+    @Autowired
+    BooksRepository repository;
+
+    public List<BookVO> findAll() {
+        return DozerConverter.parseListObjects(repository.findAll(), BookVO.class);
+    }
+
+    public BookVO findById( Long id ) {
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+        return DozerConverter.parseObject(entity, BookVO.class);
+    }
+
+    public BookVO create( BookVO book ) {
+        var entity = DozerConverter.parseObject(book, Book.class);
+
+        return  DozerConverter.parseObject(repository.save(entity), BookVO.class);
+    }
+
+    public BookVO update( BookVO book ) {
+        var entity = repository.findById(book.getKey())
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        entity.setTitle(book.getTitle());
+        entity.setAuthor(book.getAuthor());
+        entity.setLaunchDate(book.getLaunchDate());
+        entity.setPrice(book.getPrice());
+
+        return DozerConverter.parseObject(repository.save(entity), BookVO.class);
+    }
+
+    public void delete ( Long id ) {
+        var entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+
+        repository.delete(entity);
+    }
+}
