9624a595366a9b4808986c763f9f361fbd75fbb3
OakAnderson <andersonfelipe01@live.com>
2021-05-30 14:07:32 -0300

    Ordering the result with JsonProperty to a more consistent order

--- src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -1,16 +1,20 @@
 package br.com.erudio.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.github.dozermapper.core.Mapping;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+@JsonPropertyOrder({"id", "firstName", "lastName", "address", "gender"})
 public class PersonVO extends RepresentationModel<PersonVO> implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Mapping("id")
+    @JsonProperty("id")
     private Long key;
     private String address;
     private String firstName;
