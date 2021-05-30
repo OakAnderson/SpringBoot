0beba32419424dc3eef840a9ff59bb6124f1ca83
OakAnderson <andersonfelipe01@live.com>
2021-05-30 09:21:28 -0300

    Moving API to a best standard way using API as prefix in the URI and V1 as the suffix
    Now you can access the methods by:
    
    https://{address}/api/person/v1

--- src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -1,4 +1,8 @@
-package br.com.erudio.data.vo;
+package br.com.erudio.data.vo.v1;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import java.io.Serializable;
 import java.util.Objects;
