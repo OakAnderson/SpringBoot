3e7a7d8e8c8ca8a52f2b2f77988392f1ca782928
OakAnderson <andersonfelipe01@live.com>
2021-05-31 06:55:05 -0300

    Adding User Repository and Service

---
+++ src/main/java/br/com/erudio/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package br.com.erudio.repository;
+
+import br.com.erudio.data.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u where u.userName =:userName")
+    Optional<User> findByUserName(@Param("userName") String userName);
+
+}
---
+++ src/main/java/br/com/erudio/services/UserServices.java
@@ -0,0 +1,27 @@
+package br.com.erudio.services;
+
+import br.com.erudio.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServices implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    public UserServices(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUserName(username)
+                .orElseThrow(
+                        () -> new UsernameNotFoundException(
+                                "Username " + username + " not found."));
+    }
+}
