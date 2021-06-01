47286c1efef2bb36d69fcea711f83d26a0054c89
OakAnderson <andersonfelipe01@live.com>
2021-05-31 21:33:07 -0300

    Created POST method uploadMultipleFiles

--- src/main/java/br/com/erudio/controller/FileController.java
+++ src/main/java/br/com/erudio/controller/FileController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Api(tags = "FileEndpoint")
 @RestController
 @RequestMapping("/api/file/v1")
@@ -29,6 +33,13 @@ public class FileController {
                 .toUriString();
 
         return new UploadFileResponseVO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseVO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        return Arrays.stream(files)
+                .map(this::uploadFile)
+                .collect(Collectors.toList());
     }
 
 }
