baf65fe73d66ec32b92fc88125ea7d7d6e1e3383
OakAnderson <andersonfelipe01@live.com>
2021-05-31 21:50:55 -0300

    Created GET method for download files already stored in hard disc

--- src/main/java/br/com/erudio/controller/FileController.java
+++ src/main/java/br/com/erudio/controller/FileController.java
@@ -3,14 +3,18 @@ package br.com.erudio.controller;
 import br.com.erudio.data.vo.v1.UploadFileResponseVO;
 import br.com.erudio.services.FileStorageService;
 import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,6 +26,8 @@ public class FileController {
 
     @Autowired
     private FileStorageService fileStorageService;
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
 
     @PostMapping("/uploadFile")
     public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
@@ -40,6 +46,29 @@ public class FileController {
         return Arrays.stream(files)
                 .map(this::uploadFile)
                 .collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+
+        Resource resource = fileStorageService.loadFileAsResource(fileName);
+
+        String contentType = null;
+
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception e) {
+            logger.info("Could not determinate file type!");
+        }
+
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attatchment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
--- src/main/java/br/com/erudio/services/FileStorageService.java
+++ src/main/java/br/com/erudio/services/FileStorageService.java
@@ -2,7 +2,10 @@ package br.com.erudio.services;
 
 import br.com.erudio.config.FileStorageConfig;
 import br.com.erudio.exception.FileStorageException;
+import br.com.erudio.exception.MyFileNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -45,6 +48,20 @@ public class FileStorageService {
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
         }
 
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+            Resource resource = new UrlResource(filePath.toUri());
+            if( resource.exists() ) {
+                return resource;
+            } else {
+                throw new MyFileNotFoundException("File not found " + fileName);
+            }
+        } catch (Exception e) {
+            throw new MyFileNotFoundException("File not found " + fileName, e);
+        }
     }
 
 }
